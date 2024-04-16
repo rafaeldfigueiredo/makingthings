3cdfb3637cb1722a926172e2f0ff57eec7e9299d
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-04-16 13:16:32 -0300

    finish main functionalities

--- test.js
+++ test.js
@@ -1,36 +1,20 @@
-const list = document.querySelector('#list')
-
-/**
- * Interact with PókeAPI to get pokemons data
- * @param pokemon - ID of the Pokemon. Default is 1(Bulbasaur)
- * @returns data - A JSON file containing Pokemon characteristics
- */
 async function fetchPoke(pokemon=1) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
   const data = await response.json()
   return data
 }
 
-/**
- * Render a Pokemon image and name based on fetchPoke return of data.
- * @param data - The data received from fetchPoke() querying PókeAPI. 
- */
 async function renderPoke(data){
-  // Create the Pokemon display elements
-  let pokeContainer = document.createElement('figure');
-  let image = document.createElement('img');
+  clearList()
+  let pokeName = document.createTextNode(data.name);
+  const image = document.createElement('img');
   let pokesubtitle = document.createElement('figcaption')
-  var pokeName = document.createTextNode(data.name);
+  pokesubtitle.setAttribute('id','name');
 
-  //Assign each element their respectives displays
-  pokesubtitle.setAttribute('class','name'); // for CSS
-  image.src = data['sprites']['front_default'];
+  image.src = data.sprites.front_default;
   pokesubtitle.appendChild(pokeName);
-  //Insert image and captions to figure tag and, then, the latter to #list
-  pokeContainer.appendChild(image);
-  pokeContainer.appendChild(pokesubtitle);
-  list.appendChild(pokeContainer);
-  return console.log(`Renderizing ${data.name}`);
+  list.appendChild(image);
+  list.appendChild(pokesubtitle);
 } 
 
 async function getPoke(){ // main safe func
@@ -56,8 +40,10 @@ async function getPokeRand(){ // Get random one
 }
 
 function clearList(){
-  let x = document.querySelectorAll('figure')
+  let x = document.querySelectorAll('img')
+  let y = document.querySelectorAll('figcaption')
   for (let i = 0; i < x.length; i++) {
     x[i].remove()
+    y[i].remove()
   }
 }
