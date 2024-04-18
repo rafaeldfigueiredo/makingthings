9b6194e890ff2eb1ad1b789a6e55118cfb72cd94
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-04-18 19:09:53 -0300

    finish flag func and other adds
    So yeah, I made some async functions
    and one of them doesn't stop so I made
    a simple stopIt flag variable. Need to implement
    that logic to PokemonRegion.getRegionPoke() so
    it gets cleared when re-rendering

--- pokeRender/pokeFunc.js
+++ pokeRender/pokeFunc.js
@@ -1,52 +1,53 @@
-const list = document.querySelector('#list')
-let stop = false
+const list = document.querySelector("#list");
+var stopIt = false;
 /**
  * Interact with PókeAPI to get pokemons data
  * @param pokemon - ID of the Pokemon. Default is 1(Bulbasaur)
  * @returns data - A JSON file containing Pokemon characteristics
  */
-async function fetchPoke(pokemon=1) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await response.json()
-  return data
+async function fetchPoke(pokemon = 1) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).catch(err => console.log(err));
+  const data = await response.json();
+  return data;
 }
 
 /**
  * Render a Pokemon image and name based on fetchPoke return of data.
- * @param data - The data received from fetchPoke() querying PókeAPI. 
+ * @param data - The data received from fetchPoke() querying PókeAPI.
  */
-async function renderPoke(data){
+async function renderPoke(data) {
   // Create the Pokemon display elements. In hierachic order: Figure > Img(imgUrl) - Figcaption
-  let pokeContainer = document.createElement('figure');
-  let pokeImage = document.createElement('img');
-  let pokesubtitle = document.createElement('figcaption')
+  let pokeContainer = document.createElement("figure");
+  let pokeImage = document.createElement("img");
+  let pokesubtitle = document.createElement("figcaption");
   var pokeName = document.createTextNode(capit(data.name));
 
   //Assign each element their respectives displays and classes
-  pokesubtitle.setAttribute('class','name'); // for CSS
-  pokeImage.setAttribute('class','pokeImage'); // for CSS
-  pokeImage.src = data['sprites']['front_default'];
+  pokesubtitle.setAttribute("class", "name"); // for CSS
+  pokeImage.setAttribute("class", "pokeImage"); // for CSS
+  pokeImage.src = data["sprites"]["front_default"];
   pokesubtitle.appendChild(pokeName);
   //Insert image and captions to figure tag and, then, the latter to #list
   pokeContainer.appendChild(pokeImage);
   pokeContainer.appendChild(pokesubtitle);
   list.appendChild(pokeContainer);
   return console.log(`Renderizing ${data.name}`);
-} 
-
-async function getPoke(){ // main safe func
-  clearList()
-  let value = document.querySelector("#input").value //gets input value
-  let data = await fetchPoke(value); // Being numeric, fetchPoke get from db
-  renderPoke(data)
 }
- 
+
+async function getPoke() {
+  // main safe func
+  clearList();
+  let value = document.querySelector("#input").value; //gets input value
+  let data = await fetchPoke(value); // Being numeric, fetchPoke get from db
+  renderPoke(data);
+}
+
 async function keepDis() {
-  clearList()
-  stop = false
-  while (stop == false) {
-    
+  clearList();
+  stopIt = false;
+  while (stopIt == false) {
     for (let value = 1; value <= 100; value++) {
+      if (stopIt == true) {break}
       const data = await fetchPoke(value);
       renderPoke(data);
       await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait 1 second
@@ -55,21 +56,68 @@ async function keepDis() {
   console.log("All Pokemons displayed!");
 }
 
-async function getPokeRand(){ // Get random one
-  clearList()
-  renderPoke(await fetchPoke(Math.floor(Math.random() * 1000)))
+async function getPokeRand() {
+  // Get random one
+  clearList();
+  renderPoke(await fetchPoke(Math.floor(Math.random() * 1000)));
 }
 
-function clearList(){
-  stop = true
-  let x = document.querySelectorAll('figure')
-  for (let i = 0; i < x.length; i++) {
-    x[i].remove()
-  }
+function clearList() {
+  stopIt = true;
+  list.innerHTML = ''
 }
 /**
  * Just capitalizing first letter.
  */
 function capit(text) {
   return text[0].toUpperCase() + text.slice(1);
+}
+/**
+ * This class is meant to encapsulate the same logic of keepDisplaying() but
+ * with defined initial/end values based on the NationalDex ID of each
+ * region.
+ */
+class PokemonRegion {
+  /**
+   *
+   * @param {String} regionName - The Pokemon official region' name.
+   * @param {Number} startId - The first NationalDex entry of the respective region
+   * @param {Number} endId - The first NationalDex entry of the respective region
+   */
+  constructor(regionName, startId, endId, fetchPoke, renderPoke,clearList) {
+    this.startId = startId;
+    this.endId = endId;
+    this.regionName = regionName;
+    this.fetchPoke = fetchPoke;
+    this.renderPoke = renderPoke;
+    this.clearList = clearList;
+  }
+
+  async getRegPokes() {
+    clearList()
+    stopIt = false;
+    console.log(`Now rendering:${this.regionName}`);
+    while (stopIt == false) {
+      for (let value = this.startId; value <= this.endId; value++) {
+        if (stopIt == true){break}
+        const data = await this.fetchPoke(value);        
+        this.renderPoke(data);
+      }
+    }
+  }
+}
+
+function getKantoPoke() {
+  let kanto = new PokemonRegion("Kanto", 1, 151, fetchPoke, renderPoke,clearList);
+  kanto.getRegPokes();
+}
+
+function getJohtoPoke() {
+  let johto = new PokemonRegion("Johto", 152, 251, fetchPoke, renderPoke,clearList);
+  johto.getRegPokes();
+}
+
+function getHoennPoke() {
+  let hoenn = new PokemonRegion("Hoenn", 252, 386, fetchPoke, renderPoke,clearList);
+  hoenn.getRegPokes();
 }
