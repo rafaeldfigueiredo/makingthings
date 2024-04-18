b083221600bbb490c366c5086aeabaef6bf1c168
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-04-18 19:13:32 -0300

    remove modules and add gitignore

--- Metal Bands.session.sql
+++
@@ -1,9 +0,0 @@
--- @block
-SELECT band_name AS Bands,formed AS FormationYear
-FROM metal_bands
-ORDER BY formed DESC
-
--- @block
-SELECT DISTINCT band_name,origin,style FROM metal_bands
-WHERE origin='Brazil' AND split != ''
-ORDER BY band_name
