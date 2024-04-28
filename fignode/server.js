1e37981e6e610c4c08fcbd621237180342be41c4
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-04-28 16:09:36 -0300

    set host option on liste

--- fignode/server.js
+++ fignode/server.js
@@ -38,4 +38,6 @@ server.delete('/videos/:id',async (request,reply)=>{
   return reply.status(204).send()
 })
 
-server.listen({port:process.env.port ?? 3333})
+server.listen({
+  host:'0.0.0.0',
+  port:process.env.port ?? 3333})
