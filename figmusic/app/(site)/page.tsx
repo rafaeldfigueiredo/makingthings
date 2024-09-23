686016a7f1812f8ff19085c38df373366ad32996
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-09-23 19:25:15 +0000

    deleted unused files

--- figmusic/app/(site)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div className="text-green-500">
-      Hello (Site) !
-    </div>
-  );
-}
--- fignode/node22/src/components/Tweet.tsx
+++
@@ -1,7 +0,0 @@
-type TweetProps = {
-  name:string;
-}
-
-export function Tweet(props:TweetProps){
-  return <p>{props.name}</p>
-}
